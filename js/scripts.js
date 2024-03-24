ca0a7c303bbc6f7e41bc7496d70a455944321285
DrBecky22 <101670044+DrBecky22@users.noreply.github.com>
2024-03-24 11:29:45 -0400

    commit
    adding code - before class

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,18 @@ carouselPlay.addEventListener('click', function() {
     console.log('cycle the carousel');
     carousel.cycle();
 })
+
+async function fetchWeather() {
+    const apiKey = process.env.OPEN_WEATHER_API_KEY;
+    let city = "Aspen";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('there was an error', error);
+    }
+}
+
+fetchWeather();
